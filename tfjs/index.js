9d3de9e6e139faf6e2b92c4323524d135bf5a968
Ayush Rautwar <arautwar@gmail.com>
2021-01-19 20:42:40 -0500

    Add user input

--- tfjs/index.js
+++ tfjs/index.js
@@ -1,6 +1,48 @@
+
 function tanh(x) {
     e = Math.exp(2*x);
     return (e - 1) / (e + 1) ;
+}
+
+async function reval(t){
+    const model = await tf.loadGraphModel('https://www.ayushrautwar.com/tfjs/model.json');
+    var tick = document.getElementById(""+t+"f").value;
+    var time = +new Date;
+    var starttime = time-90*24*60*60;
+    url = "https://cors-anywhere.herokuapp.com/query1.finance.yahoo.com/v7/finance/chart/"+tick+"?symbol="+tick+"&interval=1d&period0="+starttime+"&period2="+time;
+    let response = await fetch(url);
+    var json;
+    if (response.ok) { // if HTTP-status is 200-299
+        // get the response body (the method explained below)
+        json = await response.json();
+    } else {
+        return;
+    }
+    l = json.chart.result[0].indicators.quote[0].close.slice(-30);
+
+    n = [0];
+    //console.log(l);
+    //console.log(l.length);
+    for (var p=0; p<l.length-1; p++){
+        n.push(tanh(10*(l[p + 1] - l[p])/l[0]));
+    }
+    //console.log(n);
+    var d = {0: "HOLD", 1: "BUY", 2: "SELL"};
+    var d2 = {0: "yellow", 1: "green", 2: "red"};
+    var out = tf.gather(model.predict(tf.tensor([n])), 0);
+    //console.log(out);
+    var ans = out.argMax();
+    var ans = ans.dataSync()[0];
+    //console.log(d2[ans]);
+    document.getElementById(""+(t)).style.borderColor= d2[ans];
+    document.getElementById(""+(t)).children[0].children[0].value=tick;
+    console.log(""+Math.round(out.arraySync()[ans]*100)+"%");
+    console.log(document.getElementById(""+(t)).children[1].children[1]);
+    var temp = ""+Math.round(out.arraySync()[ans]*100)+"%";
+    document.getElementById(""+(t)).children[1].children[1].style.width=temp;
+    document.getElementById(""+(t)).children[1].children[2].textContent=temp;
+    document.getElementById(""+(t)).children[1].children[3].textContent="confident";
+    document.getElementById(""+(t)).children[1].children[4].textContent=d[ans];
 }
 
 async function run_tickers(){
@@ -38,7 +80,7 @@ async function run_tickers(){
         var ans = ans.dataSync()[0];
         //console.log(d2[ans]);
         document.getElementById(""+(i+1)).style.borderColor= d2[ans];
-        document.getElementById(""+(i+1)).children[0].textContent=tickers[i];
+        document.getElementById(""+(i+1)).children[0].children[0].value=tickers[i];
         console.log(""+Math.round(out.arraySync()[ans]*100)+"%");
         console.log(document.getElementById(""+(i+1)).children[1].children[1]);
         var temp = ""+Math.round(out.arraySync()[ans]*100)+"%";
